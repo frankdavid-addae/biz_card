386f4cf4a8c3561077d1694b5773ce6e6997e8e0
Frank David Addae <frankdavid.addae@gmail.com>
2021-09-17 14:40:06 +0000

    project setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,14 +9,34 @@ void main() {
 class BizCardApp extends StatelessWidget {
   const BizCardApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    const kDarkColor = Color(0xFF2B292D);
+    const kGreyColor = Color(0xFFD9D9D9);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Colors.teal[900],
         body: SafeArea(
-          child: Column(),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 50.0,
+                backgroundColor: kDarkColor,
+                backgroundImage: AssetImage('assets/images/avatar.jpg'),
+              ),
+              Text(
+                'Frank David Addae',
+                style: TextStyle(
+                  color: kGreyColor,
+                  fontSize: 40.0,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Pacifico-Regular',
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
