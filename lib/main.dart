d70ed40cf8f06458a15fa2f259f993c6eaa91c08
Frank David Addae <frankdavid.addae@gmail.com>
2021-09-17 16:21:44 +0000

    added some more components and refactored some codes

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,12 @@ class BizCardApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const kDarkColor = Color(0xFF2B292D);
+    const kDarkColor = Color(0xFF23414E);
     const kGreyColor = Color(0xFFD9D9D9);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.teal[900],
+        backgroundColor: kDarkColor,
         body: SafeArea(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
@@ -33,6 +33,60 @@ class BizCardApp extends StatelessWidget {
                   fontSize: 40.0,
                   fontWeight: FontWeight.bold,
                   fontFamily: 'Pacifico-Regular',
+                ),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  letterSpacing: 2.5,
+                  color: kGreyColor.withOpacity(0.8),
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'SourceSansPro-Regular',
+                ),
+              ),
+              Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(6.0),
+                ),
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                color: kGreyColor.withOpacity(0.8),
+                child: ListTile(
+                  horizontalTitleGap: 5.0,
+                  leading: Icon(
+                    Icons.phone,
+                    color: kDarkColor,
+                  ),
+                  title: Text(
+                    '+233 24 762 7952',
+                    style: TextStyle(
+                      color: kDarkColor,
+                      fontSize: 20.0,
+                      fontFamily: 'SourceSansPro-Regular',
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(6.0),
+                ),
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                color: kGreyColor.withOpacity(0.8),
+                child: ListTile(
+                  horizontalTitleGap: 5.0,
+                  leading: Icon(
+                    Icons.email,
+                    color: kDarkColor,
+                  ),
+                  title: Text(
+                    'frankdavid.addae@gmail.com',
+                    style: TextStyle(
+                      color: kDarkColor,
+                      fontSize: 20.0,
+                      fontFamily: 'SourceSansPro-Regular',
+                    ),
+                  ),
                 ),
               ),
             ],
